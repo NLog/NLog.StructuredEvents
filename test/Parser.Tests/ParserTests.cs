ef8d5d8b73efa1636e1e0bbfd63182693c4f9beb
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-08-10 23:26:36 +0200

    more tests

--- test/Parser.Tests/ParserTests.cs
+++ test/Parser.Tests/ParserTests.cs
@@ -13,7 +13,20 @@ namespace Parser.Tests
         [InlineData("I like my {car}")]
         [InlineData("But when Im drunk I need a {cool} bike")]
         [InlineData("I have {0} {1} {2} parameters")]
-        [InlineData("I have {{text}} and {{0}}")] //todo works, but lost context
+        [InlineData("{0} on front")]
+        [InlineData(" {0} on front")]
+        [InlineData("end {1}")]
+        [InlineData("end {1} ")]
+        [InlineData("{name} is my name")]
+        [InlineData(" {name} is my name")]
+        [InlineData("{multiple}{parameters}")]
+        [InlineData("I have {{text}} and {{0}}")] 
+        [InlineData("{{text}}{{0}}")] 
+        [InlineData(" {{text}}{{0}} ")] 
+        [InlineData(" {0} ")] 
+        [InlineData(" {1} ")] 
+        [InlineData(" {2} ")] 
+        [InlineData(" {3} {4} {9} {8} {5} {6} {7}")] 
         public void TestSimple(string input)
         {
             var parser = new TemplateParser();
