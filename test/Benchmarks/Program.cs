9a22bd152b7b60875102530d9186effa55a47f4a
jods <joel.dumas@elca.ch>
2016-08-15 21:20:50 +0200

    Benchmarking project.

---
+++ test/Benchmarks/Program.cs
@@ -0,0 +1,18 @@
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Parser;
+
+public class Program 
+{
+    public static void Main()
+    {
+        BenchmarkRunner.Run<Program>();
+    }
+
+    [Params("Whatever {subject} you want to benchmark in {time}ms.",
+            "{time:yyyy-MM-dd HH:mm:ss} {severity} {message}.")]
+    public string Text { get; set; }
+
+    [Benchmark]
+    public Template Parse() => TemplateParser.Parse(Text);
+}
