13c7005261049415b94e077b3bf39817ccd005d5
Julian Verdurmen <304NotModified@users.noreply.github.com>
2017-01-26 23:00:29 +0100

    enable strong naming

---
+++ test/NLog.StructuredEvents.Tests/TestExt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog.StructuredEvents.Serialization;
+
+namespace NLog.StructuredEvents.Tests
+{
+    public static class TestExt
+    {
+        /// <summary>
+        /// Helper for testing
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        internal static string SerializeObject(this SerializationManager manager, object value)
+        {
+            var sb = new StringBuilder();
+
+            manager.SerializeObject(sb, value, CultureInfo.InvariantCulture);
+            return sb.ToString();
+        }
+    }
+}
